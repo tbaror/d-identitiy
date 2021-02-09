5c0596b72a867ecfef6b7e19c82a8cb40c4c4751
tbaror <tal_baror@hotmail.com.com>
2021-02-09 22:51:49 +0200

    commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,43 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 #from .models import User
 
 
 
-class UserLoginForm(forms.ModelForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'type':'email', 'name':'Username', 'placeholder':'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password', 'name': 'password', 'placeholder':'Password'}))
+class UserLoginForm(UserCreationForm):
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password', 'name': 'password', 'placeholder':'Password'}),
+    )
+    #username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'type':'email', 'name':'Username', 'placeholder':'Username'}))
+   # password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password', 'name': 'password', 'placeholder':'Password'}))
 
-    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        widgets={
+            'username': forms.TextInput(attrs={'class': 'form-control', 'type':'email', 'name':'Username', 'placeholder':'Username'}),
+            
+        }
+
+
+class CreateUserForm(UserCreationForm):
+
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={'class':'pass', 'type':'password', 'align':'center', 'placeholder':'password'}),
+    )
+    password2 = forms.CharField(
+        label="Confirm password",
+        widget=forms.PasswordInput(attrs={'class':'pass', 'type':'password', 'align':'center', 'placeholder':'password'}),
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        widgets={
+            'username': forms.TextInput(attrs={'class':'un', 'type':'text', 'align':'center', 'placeholder':'UserName'}),
+            'email':forms.TextInput(attrs={'class':'un', 'type':'text', 'align':'center', 'placeholder':'Email'}),
+        }
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import *
+import ldap3
 
 
 class UsersLoginView(LoginView):
@@ -16,6 +17,63 @@ class UsersLoginView(LoginView):
 class UserProfileView(TemplateView):
     template_name = 'user-profile.html'    
 
+
+class ChangeUserPassword(CreateView):
+    info_sended = False
+    template_name = "changepassword.html"
+    form_class = ContactRegisterForm
+    success_url = reverse_lazy('loader_success')
+
+    def get_initial(self):
+        # Get form data from customer
+        initial = super().get_initial()
+        initial['name'] = self.request.POST.get('name')
+        initial['mail'] = self.request.POST.get('mail')
+        initial['tel_num'] = self.request.POST.get('tel_num')
+        initial['message_contact'] = self.request.POST.get('message_contact')
+        initial['contact_date'] = self.request.POST.get('contact_date')
+        # Send Mail Rutine
+        messege_subject = 'You got New Messege contact from {0}'.format(initial['name'])
+        message_contact = ' שלום מ {0} \n {1} \n {2} \n טלפון ליצירת קשר: {3}'.format(initial['name'], initial['mail'], initial['message_contact'], initial['tel_num'])
+        mail_from = initial['mail']
+        send_mail(
+            messege_subject,
+            message_contact,
+            mail_from,
+            ['carnepremiumgrill@gmail.com', 'tbaror@gmail.com', 'shahar130313@gmail.com']
+
+        )
+
+        return initial
+        SERVER='212.143.237.33'
+BASEDN="DC=dalet,DC=local"
+USER="pwmtest@dalet.com"
+CURREENTPWD="Esprit@1234"
+NEWPWD="Gfn@12345"
+
+SEARCHFILTER='(&(|(userPrincipalName='+USER+')(samaccountname='+USER+')(mail='+USER+'))(objectClass=person))'
+
+USER_DN=""
+
+ldap_server = ldap3.Server(SERVER, get_info=ldap3.ALL)
+conn = ldap3.Connection(ldap_server, USER, CURREENTPWD, auto_bind=True)
+conn.start_tls()
+
+print(conn)
+
+conn.search(search_base = BASEDN,
+         search_filter = SEARCHFILTER,
+         search_scope = ldap3.SUBTREE,
+         attributes = ['cn', 'givenName'],
+         paged_size = 5)
+
+for entry in conn.response:
+    if entry.get("dn") and entry.get("attributes"):
+        if entry.get("attributes").get("cn"):
+            USER_DN=entry.get("dn")
+
+print (USER_DN)
+print(ldap3.extend.microsoft.modifyPassword.ad_modify_password(conn, USER_DN, NEWPWD, CURREENTPWD,  controls=None))
 """ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
