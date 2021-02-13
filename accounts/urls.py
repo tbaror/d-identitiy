a82a015a23e32c9d7649ee877104d9bea1b29dda
tbaror <tal_baror@hotmail.com.com>
2021-02-13 22:20:07 +0200

    progress

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ from .views import *
 urlpatterns = [
     path('', auth_views.LoginView.as_view(), name = 'login'),
     path('profile', UserProfileView.as_view(), name='profile'),
-    path('changepass', ChangeUserPassword.as_view(), name="changepass"),
+    path('changepass/', ChangeUserPassword.as_view(), name="changepass"),
 
     # path('signup/', SignUpView.as_view(), name='signup'),
 ]
