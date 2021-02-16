a6f6b68f836e1ad2be1f2d8c50a9ff5e8c6d7522
tbaror <tal_baror@hotmail.com.com>
2021-02-16 22:42:39 +0200

    view progress

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
+from django.shortcuts import render
 from django.views import View
 from django.urls import reverse_lazy
 from django.views import generic
@@ -21,8 +22,27 @@ class UserProfileView(TemplateView):
     template_name = 'user-profile.html'    
 
 
-class ChangeUserPassword(TemplateView):
+class ChangeUserPassword(View):
     info_sended = False
     template_name = "changepassword.html"
+    AUTH_SERVER = config('AUTH_SERVER')
+    BASEDN = config('BASEDN')
     #form_class = UserChangePasswordForm
     #success_url = reverse_lazy('loader_success')
+
+    def get(self, request):
+        current_user = request.user
+        context = {'form' : current_user}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        PassForm = ""
+        #form = PassForm()
+        if request.method == 'POST':
+            form = PassForm(request.POST)
+
+            if form.is_valid():
+                current_user = request.user
+                print(current_user)
+
+
