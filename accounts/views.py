0fdddd03341539b4063ee965d8be87042389a460
tbaror <tal_baror@hotmail.com.com>
2021-03-02 00:08:20 +0200

    reset pass progress

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,8 @@ from django.views import View
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import *
-import ldap3
+#import ldap3
+from ldap3 import *
 from decouple import config
 import ssl
 
@@ -28,6 +29,7 @@ class ChangeUserPassword(View):
     info_sended = False
     template_name = "changepassword.html"
     AUTH_SERVER = config('AUTH_SERVER')
+    AUTH_SRV = config('AUTH_SRV')
     BASEDN = config('BASEDN')
     #form_class = UserChangePasswordForm
     success_url = reverse_lazy('profile')
@@ -53,26 +55,29 @@ class ChangeUserPassword(View):
                 SEARCHFILTER='(&(|(userPrincipalName='+USER+')(samaccountname='+USER+')(mail='+USER+'))(objectClass=person))'
 
                 USER_DN=""
+                user_attrb = ""
+                tls_configuration = Tls(validate=ssl.CERT_REQUIRED, version=ssl.PROTOCOL_TLSv1_2)
+                s = Server(self.AUTH_SRV, get_info=ALL, use_ssl=True, tls=tls_configuration)
+                c = Connection(s, user=USER, password=CURREENTPWD)
+                c.open()
+                c.bind()
 
-                ldap_server = ldap3.Server(self.AUTH_SERVER, get_info=ldap3.ALL)
-                conn = ldap3.Connection(ldap_server, USER, CURREENTPWD, auto_bind=True)
-                conn.start_tls()
 
-                print(conn)
-
-                conn.search(search_base = self.BASEDN,
+                c.search(search_base = self.BASEDN,
                 search_filter = SEARCHFILTER,
-                search_scope = ldap3.SUBTREE,
-                attributes = ['cn', 'givenName'],
-                paged_size = 5)
+                    search_scope = SUBTREE,
+                    attributes = ['cn', 'givenName'],
+                    paged_size = 5)
 
-                for entry in conn.response:
+                for entry in c.response:
                     if entry.get("dn") and entry.get("attributes"):
+        
                         if entry.get("attributes").get("cn"):
                             USER_DN=entry.get("dn")
+                
+                print (c.extend.microsoft.modify_password(USER_DN, NEWPWD, CURREENTPWD))
                 messages.success(self.request, 'Password has been changed successfully.')
-                print (USER_DN)
-                print (ldap3.extend.microsoft.modifyPassword.ad_modify_password(conn, USER_DN, NEWPWD, CURREENTPWD,  controls=None))
+                
             except Exception as e:
                 messages.warning(self.request, e)
                 print(e)
@@ -96,13 +101,54 @@ class ResetPass(View):
 
 
     def post(self, request):
+        template_name = "resetform.html"
         if request.method == 'POST':
             first_name = request.POST.get('first-name')
             last_name  = request.POST.get('last-name')
             email = request.POST.get('email')
             message = request.POST.get('message')
-
+            print(email)
+#Get User & validate info from AD
             try:
+                #Try to connect
+                USER_DN = ""
+                user_attrib = []
+                tls_configuration = Tls(validate=ssl.CERT_REQUIRED, version=ssl.PROTOCOL_TLSv1_2)
+                s = Server(self.AUTH_SRV, get_info=ALL, use_ssl=True, tls=tls_configuration)
+                c = Connection(s, user=self.SVCUSER, password=self.SVCPASS)
+                c.open()
+                c.bind()
+                #defien search filter for user account
+                SEARCHFILTER='(&(|(userPrincipalName='+email+')(samaccountname='+email+')(mail='+email+'))(objectClass=person))'
+                #Get user DN
+                c.search(search_base = self.BASEDN,
+                    search_filter = SEARCHFILTER,
+                    search_scope = SUBTREE,
+                    attributes = ['cn', 'givenName'],
+                    paged_size = 5)
 
+                for entry in c.response:
+                    if entry.get("dn") and entry.get("attributes"):
+        
+                        if entry.get("attributes").get("cn"):
+                            USER_DN=entry.get("dn")
+                #retrieve User attributes
+                c.search(
+                    search_base=USER_DN,
+                    search_filter= '(objectClass=*)', # required
+                    search_scope=BASE,
+                    attributes='*'
+                    )
 
-            
+                user_attrib[0] = c.entries[0].mail.values
+                user_attrib[1] = c.entries[0].givenName.values
+                user_attrib[2] = c.entries[0].sn.values
+                print(user_attrib[2])
+                c.unbind()
+                if email == user_attrib[0] and first_name == user_attrib[1] and last_name == user_attrib[2]:
+                    print("match")
+                else:
+                    print('not match')
+                return render(request, template_name)
+            except Exception as e:
+                print(e)        
