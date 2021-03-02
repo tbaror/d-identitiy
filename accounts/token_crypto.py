ca02573bf24354610f03b64d1ddc8ac27e7f7609
tbaror <tal_baror@hotmail.com.com>
2021-03-02 22:20:54 +0200

    otp progress

--- accounts/token_crypto.py
+++ accounts/token_crypto.py
@@ -3,11 +3,12 @@ import pyotp
 from fernet import *
 from decouple import config
 
-
+OTP_NUMLEN=config('OTP_NUMLEN')
+SECRET_KEY = config('SECRET_KEY')
 # function for otp generation
 def otpgen():
     otp=""
-    for i in range(OTP_NUMLEN=config('OTP_NUMLEN')):
+    for i in range(OTP_NUMLEN):
         otp+=str(r.randint(1,9))
     
     return otp
@@ -15,7 +16,7 @@ def otpgen():
 
 def crypt_token():
     encoded_message = otpgen().encode
-    key = config('SECRET_KEY').encode
+    key = SECRET_KEY.encode
     encryption_type = Fernet(key)
 
     encrypted_message = encryption_type.encrypt(encoded_message)
