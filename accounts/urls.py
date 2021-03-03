2cd0f4ecbbfced27aafc3cf6a92248a30c9267de
tbaror <tal_baror@hotmail.com.com>
2021-03-03 20:50:15 +0200

    totp progress

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('', auth_views.LoginView.as_view(), name = 'login'),
     path('profile', UserProfileView.as_view(), name='profile'),
     path('changepass/', ChangeUserPassword.as_view(), name="changepass"),
-    path("resetpass/", ResetPass.as_view(), name="resetpass")
+    path("resetpass/", ResetRequestForm.as_view(), name="resetpass"),
+    path("tokenchalenge/", .as_view(), name="")
 
     # path('signup/', SignUpView.as_view(), name='signup'),
 ]
