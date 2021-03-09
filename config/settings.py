53296336f57838da2db14756b7e96b45b8b3cff7
tbaror <tal_baror@hotmail.com.com>
2021-03-09 12:38:39 +0200

    all

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['212.143.237.19']
 
 
 # Application definition
