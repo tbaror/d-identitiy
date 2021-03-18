03fd78b7037a5bb271bccb3f2cf1ba32c35c0514
tal_bar_or <tbaror@gmail.com>
2021-03-18 22:33:42 +0200

    ldap ops start

---
+++ accounts/user_ops.py
@@ -0,0 +1,22 @@
+
+
+
+class LdapOpertions():
+
+    def __init__(self, SvcUser, SvcPassword, RequestUser, RequestUserPass=None, LdapServer, SearchFilter):
+
+        self.SvcUser = SvcUser
+        self.SvcPassword = SvcPassword
+        self.RequestUser = RequestUser
+        self.RequestUserPass = RequestUserPass
+        self.LdapServer = LdapServer
+        self.SearchFilter = SearchFilter
+    
+    def query_user_attrib(self):
+        pass
+
+
+
+
+        
+        
