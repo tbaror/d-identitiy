50250568dddc6b5ac28e6236c099dcd5b8d9bdc7
tbaror <tal_baror@hotmail.com.com>
2021-03-21 11:24:31 +0200

    profile progress

--- accounts/user_ops.py
+++ accounts/user_ops.py
@@ -3,17 +3,55 @@
 
 class LdapOpertions():
 
-    def __init__(self, SvcUser, SvcPassword, RequestUser, RequestUserPass=None, LdapServer, SearchFilter):
+    def __init__(self, SvcUser, SvcPassword, RequestUser, RequestUserPass=None, LdapServer, BaseDn):
 
         self.SvcUser = SvcUser
         self.SvcPassword = SvcPassword
         self.RequestUser = RequestUser
         self.RequestUserPass = RequestUserPass
         self.LdapServer = LdapServer
-        self.SearchFilter = SearchFilter
+        self.BaseDn = BaseDn
+        
     
     def query_user_attrib(self):
-        pass
+        try:
+            #Try to connect
+            USER_DN = ""
+            user_attrib = []
+            tls_configuration = Tls(validate=ssl.CERT_REQUIRED, version=ssl.PROTOCOL_TLSv1_2)
+            s = Server(self.LdapServer, get_info=ALL, use_ssl=True, tls=tls_configuration)
+            c = Connection(s, user=self.SvcUser, password=self.SvcPassword)
+            c.open()
+            c.bind()
+            #defien search filter for user account
+            SEARCHFILTER='(&(|(userPrincipalName='+self.RequestUser+')(samaccountname='+self.RequestUser+')(mail='+self.RequestUser+'))(objectClass=person))'
+            #Get user DN
+            c.search(search_base = self.BaseDn,
+            search_filter = SEARCHFILTER,
+            search_scope = SUBTREE,
+            attributes = ['cn', 'givenName'],
+            paged_size = 5)
+
+            for entry in c.response:
+                if entry.get("dn") and entry.get("attributes"):
+                    if entry.get("attributes").get("cn"):
+                        USER_DN=entry.get("dn")
+                        #retrieve User attributes
+                        c.search(
+                            search_base=USER_DN,
+                            search_filter= '(objectClass=*)', # required
+                            search_scope=BASE,
+                            attributes='*'
+                            )
+
+                        user_email = c.entries[0].mail.values
+                        user_givenname = c.entries[0].givenName.values
+                        user_sn = c.entries[0].sn.values
+                
+                        c.unbind()
+                        
+
+        
 
 
 
